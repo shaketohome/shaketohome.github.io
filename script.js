c7e0109a8d9a3c850867a062879bb3557e101f3a
shaketohome <calltoshake@gmail.com>
2026-03-31 22:34:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-function order(item) {
-  let phone = "917702622925";
-  let msg = "I want to order: " + item;
+let cart = [];
+let userLocation = "";
 
-  let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(msg);
-  window.open(url, "_blank");
+function addToCart(name, price) {
+  cart.push({name, price});
+  document.getElementById("cartText").innerText = cart.length + " items";
+}
+
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(pos => {
+    let lat = pos.coords.latitude;
+    let lon = pos.coords.longitude;
+
+    userLocation = "https://maps.google.com/?q=" + lat + "," + lon;
+
+    document.getElementById("locationText").innerText = "📍 Location set";
+  });
+}
+
+function checkout() {
+  if(cart.length === 0){
+    alert("Cart empty");
+    return;
+  }
+
+  let msg = "Order:%0A";
+  let total = 0;
+
+  cart.forEach(i=>{
+    msg += i.name + " ₹" + i.price + "%0A";
+    total += i.price;
+  });
+
+  msg += "%0ATotal: ₹" + total;
+
+  if(userLocation){
+    msg += "%0ALocation: " + userLocation;
+  }
+
+  window.open("https://wa.me/917702622925?text=" + msg);
 }
